b223b7f7efd6c3ad2db48aa78ff3335b492b8c1e
shashank-mugiwara <shashank.j@creditsaison-in.com>
2022-08-18 00:45:23 +0530

    Initial project
    Initial Project

---
+++ lib/dupfile.ex
@@ -0,0 +1,18 @@
+defmodule Dupfile do
+  @moduledoc """
+  Documentation for `Dupfile`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Dupfile.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/dupfile_test.exs
@@ -0,0 +1,8 @@
+defmodule DupfileTest do
+  use ExUnit.Case
+  doctest Dupfile
+
+  test "greets the world" do
+    assert Dupfile.hello() == :world
+  end
+end
