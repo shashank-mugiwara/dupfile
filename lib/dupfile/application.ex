8c61c9dd5a7cee5cf10830248e755b3e9d68329a
shashank-mugiwara <shashank.j@creditsaison-in.com>
2022-08-20 06:41:29 +0000

    Added pathfinder module

--- lib/dupfile/application.ex
+++ lib/dupfile/application.ex
@@ -5,7 +5,7 @@ defmodule Dupfile.Application do
 
   @impl true
   def start(_type, _args) do
-    children = [Dupfile.Results]
+    children = [Dupfile.Results, {Dupfile.Pathfinder, "."}]
 
     opts = [strategy: :one_for_one, name: Dupfile.Supervisor]
     Supervisor.start_link(children, opts)
